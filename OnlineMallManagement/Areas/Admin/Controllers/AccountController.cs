d58905cb1efd730fe30f31206871d2209847af37
Đen Long <nhokimlien1411@gmail.com>
2021-08-23 23:17:03 +0700

    thêm AccountController và AccountModel

---
+++ OnlineMallManagement/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using OnlineMallManagement.Areas.Admin.Data;
+using OnlineMallManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OnlineMallManagement.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private DBOnlineMallEntities dbContext = new DBOnlineMallEntities();
+        private AccountModel accModel = new AccountModel();
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var passMd5h = accModel.MD5Hash(model.password);
+                var data = dbContext.Admins.Where(x => x.UserName == model.username && x.Password == passMd5h).ToList();
+                if (data.Count() > 0)
+                {
+                    FormsAuthentication.SetAuthCookie(model.username, model.RememberMe);
+                    Session["username"] = data.FirstOrDefault().UserName;
+                    Session["id"] = data.FirstOrDefault().Id;
+
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Tài khoản không chính xác!");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ OnlineMallManagement/Areas/Admin/Data/AccountModel.cs
@@ -0,0 +1,52 @@
+﻿using OnlineMallManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace OnlineMallManagement.Areas.Admin.Data
+{
+    public class AccountModel
+    {
+
+        public AccountModel()
+        {
+            
+        }
+
+        public string MD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            //compute hash from the bytes of text  
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+
+            //get hash result after compute it  
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                //change it into 2 hexadecimal digits  
+                //for each byte  
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
+        }
+
+    }
+
+    public class LoginViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Vui lòng nhập tên tài khoản!")]
+        public string username { set; get; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Vui lòng nhập mật khẩu!")]
+        public string password { set; get; }
+        public bool RememberMe { set; get; }
+    }
+}
